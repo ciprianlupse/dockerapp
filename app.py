124eba5130130683459effad66e31fa66539eae3
Ciprian Lupse <ciprian.lupse@gmail.com>
2026-04-23 17:15:39 +0300

    Add environment variable support

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from deployed Docker app 🚀"
+    message = os.getenv("MESSAGE", "Default message 🚀")
+    return message
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
