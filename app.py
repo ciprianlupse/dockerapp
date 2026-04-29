2ef5caccc25814dc61deefca71545e75d7a38e86
Ciprian Lupse <ciprian.lupse@gmail.com>
2026-04-29 11:32:29 +0300

    Add secret support

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     message = os.getenv("MESSAGE", "Default message 🚀")
-    return message
+    secret = os.getenv("SECRET_MESSAGE", "No secret")
+    return f"{message} | {secret}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
