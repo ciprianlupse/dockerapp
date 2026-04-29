98c56cf40904d4951bbc6d953d38e8b3397ed949
Ciprian Lupse <ciprian.lupse@gmail.com>
2026-04-29 15:32:34 +0300

    Add health endpoint

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
     secret = os.getenv("SECRET_MESSAGE", "No secret")
     return f"{message} | {secret}"
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
